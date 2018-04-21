d66f470944b448a6c5b0fa3a08dfcb5e6c1bb2da
Yuta_Ushizima <register@yuta-u.com>
2018-04-21 13:20:04 +0000

    404 Not Foundの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,23 +11,31 @@ class ApplicationController < ActionController::Base
   rescue_from Exception, with: :rescue500
   rescue_from Forbidden, with: :rescue403
   rescue_from IpAddressRejected, with: :rescue403
+  rescue_from ActionController::RoutingError, with: :rescue404
   
   private
-  def set_layout
-    if params[:controller].match(%r{(staff|admin|customer)\b})
-      Regexp.last_match[1]
-    else
-      "customer"
+  
+    def set_layout
+      if params[:controller].match(%r{(staff|admin|customer)\b})
+        Regexp.last_match[1]
+      else
+        "customer"
+      end
     end
-  end
-  
-  def rescue403(e)
-    @exception = e
-    render "errors/forbidden", status: 403
-  end
-  
-  def rescue500(e)
-    @exception = e
-    render "errors/internal_server_error", status: 500
-  end
+    
+    def rescue403(e)
+      @exception = e
+      render "errors/forbidden", status: 403
+    end
+    
+    def rescue404(e)
+      @exception = e
+      render "errors/not_found", status: 404
+    end
+    
+    def rescue500(e)
+      @exception = e
+      render "errors/internal_server_error", status: 500
+    end
+    
 end

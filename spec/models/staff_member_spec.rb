9efce551ff7b44e4c4d4ac9ee73f41fcdd77ec60
Yuta_Ushizima <register@yuta-u.com>
2018-04-22 02:03:06 +0000

    ハッシュ関数を使った処理の実装とSpecの記述

--- spec/models/staff_member_spec.rb
+++ spec/models/staff_member_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe StaffMember, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#password=" do
+    it "文字列を与えると、hashed_passwordは長さ60の文字列になる" do
+      member  = StaffMember.new
+      member.password = "baukis"
+      # be_kind_ofはターゲット(=以下の場合はmember.hashed_passwrd)が、
+      # ()の中に指定したクラス(=以下の場合はStringクラス)のインスタンスであるかどうかを調べるマッチャー。
+      expect(member.hashed_password).to be_kind_of(String)
+      expect(member.hashed_password.size).to eq(60)
+    end
+    
+    it "nilを与えると,hashed_passwordはnilになる" do
+      member = StaffMember.new(hashed_password: 'x')
+      member.password = nil
+      # be_nilはターゲット(=以下の場合はmember.hashed_password)がnilであることを確かめるマッチャー。
+      expect(member.hashed_password).to be_nil
+    end
+  end
 end

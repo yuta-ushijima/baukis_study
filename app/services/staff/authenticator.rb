03159ebe31fd81466dfa3d6f588453bde2b923bf
Yuta_Ushizima <register@yuta-u.com>
2018-04-23 14:26:20 +0000

    サービスオブジェクトとSpec

---
+++ app/services/staff/authenticator.rb
@@ -0,0 +1,14 @@
+class Staff::Authenticator
+  def initialize(staff_member)
+    @staff_member = staff_member
+  end
+  
+  def authenticate(raw_password)
+    @staff_member && #@staff_memberがnilではなく
+      !@staff_member.suspended? && #@staff_memberが有効で
+      @staff_member.hashed_password && #@staff_memberにパスワードが設定されてて
+      @staff_member.start_date <= Date.today && #@staff_memberの開始日が今日以前で
+      (@staff_member.end_date.nil? || @staff_member.end_date > Date.today) && #@staff_memberの終了日が設定されていないか、今日よりも後で
+      BCrypt::Password.new(@staff_member.hashed_password) == raw_password #@staff_memberのパスワードが正しければtrueを返す
+  end
+end

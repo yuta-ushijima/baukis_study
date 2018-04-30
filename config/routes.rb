55339cd80d30e70b69660fe9f137997f9e308583
Yuta_Ushizima <register@yuta-u.com>
2018-04-30 08:35:21 +0000

    routesの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
   
-  namespace :staff do
+  namespace :staff, path: '' do
     root "top#index"
     get 'login' => 'sessions#new', as: :login
-    post 'session' => 'sessions#create', as: :session
-    delete 'session' => 'sessions#destroy'
+    resource :session, only: [ :create, :destroy ]
+    resource :account, expect: [ :new, :create, :destroy ]
   end
   
   namespace :admin do
     root "top#index"
     get 'login' => 'sessions#new', as: :login
-    post 'session' => 'sessions#create', as: :session
-    delete 'session' => 'sessions#destroy'
+    resource :session, only: [ :create, :destroy ]
+    resources :staff_members
   end
   
   namespace :customer do

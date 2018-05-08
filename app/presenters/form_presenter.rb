06ed432b6e95d570add30d0876236bf466712ea7
Yuta_Ushijima <register@yuta-u.com>
2018-05-08 05:16:37 +0000

    ERBテンプレート内でHtmlBuilderを使う

--- app/presenters/form_presenter.rb
+++ app/presenters/form_presenter.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 class FormPresenter
   include HtmlBuilder
 
@@ -9,4 +10,37 @@ class FormPresenter
    @form_builder = form_builder
    @view_context = view_context
  end
+
+ def notes
+   markup(:dev, class: 'notes') do |m|
+     m.span '*', class: 'mark'
+     m.text '印のついた項目は入力必須です。'
+   end
+ end
+
+ def text_field_block(name, label_text, options = {})
+   markup(:div, class: 'input-block') do |m|
+     m << decorated_label(name, label_text, options)
+     m << text_field(name, options)
+   end
+ end
+
+ def password_field_block(name, label_text, options = {})
+   markup(:div, class: 'input-block') do |m|
+     m << decorated_label(name, label_text, options)
+     m << password_field(name, options)
+   end
+ end
+
+ def date_field_block(name, label_text, options = {})
+   markup(:div, class: 'input-block') do |m|
+     m << decorated_label(name, label_text, options)
+     m  << text_field(name, options)
+   end
+ end
+
+ private
+    def decorated_label(name, label_text, options = {})
+      label(name, label_text, class: options [:required] ? 'required' : nil)
+    end
 end

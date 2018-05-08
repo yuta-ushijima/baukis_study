bdb01ae0535399cd6f6225624bc5d4769a62f09f
Yuta_Ushijima <register@yuta-u.com>
2018-05-08 06:04:58 +0000

    エラーメッセージの生成

--- app/presenters/form_presenter.rb
+++ app/presenters/form_presenter.rb
@@ -22,6 +22,7 @@ class FormPresenter
    markup(:div, class: 'input-block') do |m|
      m << decorated_label(name, label_text, options)
      m << text_field(name, options)
+     m << error_messages_for(name)
    end
  end
 
@@ -29,6 +30,7 @@ class FormPresenter
    markup(:div, class: 'input-block') do |m|
      m << decorated_label(name, label_text, options)
      m << password_field(name, options)
+     m << error_messages_for(name)
    end
  end
 
@@ -36,6 +38,17 @@ class FormPresenter
    markup(:div, class: 'input-block') do |m|
      m << decorated_label(name, label_text, options)
      m  << text_field(name, options)
+     m << error_messages_for(name)
+   end
+ end
+
+ def error_messages_for(name)
+   markup do |m|
+     object.errors.full_messages_for(name).each do |message|
+       m.div(class: 'error-message') do |m|
+         m.text message
+       end
+     end
    end
  end
 

249af227f91cd0a738d8c142a68580bb4003dd01
Yuta_Ushijima <register@yuta-u.com>
2018-05-08 04:02:52 +0000

    HtmlBuilderの導入

--- app/presenters/model_presenter.rb
+++ app/presenters/model_presenter.rb
@@ -1,7 +1,8 @@
 class ModelPresenter
+  include HtmlBuilder
   attr_reader :object, :view_context
   # 引数に指定した名前のインスタンスメソッドを定義し、toオプションで指定したメソッドに振る舞いを任せる
-  delegate :raw, to: :view_context
+  delegate :raw, :link_to, to: :view_context
   # object/view_contextは読みだし専用
 
   def initialize(object, view_context)

507e28e51e58e838f7357c7a6d7217256b8bf122
Yuta_Ushijima <register@yuta-u.com>
2018-05-09 02:00:03 +0000

    顧客アカウントの一覧表示・詳細表示/シードデータの投入

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,7 @@
 class Customer < ActiveRecord::Base
 
-  has_one :home_address, dependent: :destory # home_addressとcustomerを1対1の関連付け
-  has_one :work_address, dependent: :destory # work_addressとcustomerを1対1の関連付け
+  has_one :home_address, dependent: :destroy # home_addressとcustomerを1対1の関連付け
+  has_one :work_address, dependent: :destroy # work_addressとcustomerを1対1の関連付け
 
   before_validation do
     self.email_for_index = email.downcase if email
@@ -9,7 +9,7 @@ class Customer < ActiveRecord::Base
   end
 
   def password=(raw_password)
-    if raw_password.kind_of(String)
+    if raw_password.kind_of?(String)
       self.hashed_password = BCrypt::Password.create(raw_password)
     elsif raw_password.nil?
       self.hashed_password = nil

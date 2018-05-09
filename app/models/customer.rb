bd469ff02dceccd63a4559a478fd43d64f0d3d10
Yuta_Ushijima <register@yuta-u.com>
2018-05-09 00:28:38 +0000

    顧客関連モデル群の初期実装

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,18 @@
 class Customer < ActiveRecord::Base
+
+  has_one :home_address, dependent: :destory # home_addressとcustomerを1対1の関連付け
+  has_one :work_address, dependent: :destory # work_addressとcustomerを1対1の関連付け
+
+  before_validation do
+    self.email_for_index = email.downcase if email
+    self
+  end
+
+  def password=(raw_password)
+    if raw_password.kind_of(String)
+      self.hashed_password = BCrypt::Password.create(raw_password)
+    elsif raw_password.nil?
+      self.hashed_password = nil
+    end
+  end
 end

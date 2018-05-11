ae40ec1756c9271278c1421cac1c686fc6665a7c
Yuta_Ushijima <register@yuta-u.com>
2018-05-11 04:45:36 +0000

    Capybaraのテスト

--- spec/features/staff/customer_management_spec.rb
+++ spec/features/staff/customer_management_spec.rb
@@ -12,6 +12,27 @@ feature '職員による顧客管理' do
     login_as_staff_member(staff_member)
   end
 
+  scenario '職員が顧客(基本情報のみ)を登録する' do
+    click_link '顧客管理'
+    first('div.links').click_link '新規登録'
+    fill_in 'メールアドレス', with: 'test@example.jp'
+    fill_in 'パスワード', with: 'pw'
+    fill_in 'form_customer_family_name', with: '試験'
+    fill_in 'form_customer_given_name', with: '華子'
+    fill_in 'form_customer_family_name_kana', with: 'シケン'
+    fill_in 'form_customer_given_name_kana', with: 'ハナコ'
+    fill_in '生年月日', with: '1980-05-12'
+    choose '女性'
+    click_button '登録'
+
+    new_customer = Customer.order(:id).last
+    expect(new_customer.email).to eq('test@example.jp')
+    expect(new_customer.birthday).to eq(Date.new(1980,5,12))
+    expect(new_customer.gender).to eq('female')
+    expect(new_customer.home_address).to be_nil
+    expect(new_customer.work_address).to be_nil
+  end
+
   scenario '職員が顧客、自宅住所、勤務先を登録する' do
     click_link '顧客管理'
     first('div.links').click_link '新規登録'
@@ -24,6 +45,7 @@ feature '職員による顧客管理' do
     fill_in 'form_customer_given_name_kana', with: 'ハナコ'
     fill_in '生年月日', with: '1970-01-01'
     choose '女性'
+    check '自宅住所を入力する'
     within('fieldset#home-address-fields') do
       fill_in '郵便番号', with: '1000001'
       select '東京都', from: '都道府県'
@@ -31,6 +53,7 @@ feature '職員による顧客管理' do
       fill_in '町域、番地など', with: '千代田1-1-1'
       fill_in '建物名、部屋番号など', with: ' '
     end
+    check '勤務先を入力する'
     within('fieldset#work-address-fields') do
       fill_in '会社名', with: 'テスト'
       fill_in '部署名', with: ' '
@@ -83,6 +106,6 @@ feature '職員による顧客管理' do
       expect(page).to have_css(
         'div.field_with_errors input#form_customer_birthday')
       expect(page).to have_css(
-        'div.field_with_errors input#form_home_address_postal_code')
+        'div.field_with_errors input#form_home_address_postal_code', visible: false)
   end
 end

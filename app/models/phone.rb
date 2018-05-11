1c8417c99b4d749fdc13e74b928348c00a730355
Yuta_Ushijima <register@yuta-u.com>
2018-05-11 08:47:03 +0000

    phonesテーブルとphoneモデルの実装2

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -1,2 +1,15 @@
 class Phone < ActiveRecord::Base
+  include StringNormalizer
+
+  belongs_to :customer
+  belongs_to :address
+
+  before_validation do
+    self.number = normalize_as_phone_number(rnumber)
+    self.number_for_index = number.gsub(/\D/, ' ') if number
+  end
+
+  before_create do
+    self.customer = address.customer if address
+  end
 end

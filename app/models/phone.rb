addbd7cf51301b39d854d9a0dbde0132525b03e1
Yuta_Ushijima <register@yuta-u.com>
2018-05-11 09:32:41 +0000

    顧客、自宅住所、勤務先との関連づけ

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -5,11 +5,20 @@ class Phone < ActiveRecord::Base
   belongs_to :address
 
   before_validation do
-    self.number = normalize_as_phone_number(rnumber)
+    self.number = normalize_as_phone_number(number)
+    # \Dの正規表現で数字以外の文字列に全てマッチさせる
+    # gsubメソッドの第二引数に空白を入れて、全て除去
     self.number_for_index = number.gsub(/\D/, ' ') if number
   end
 
   before_create do
+    # オブジェクトが初めてDBに保存される前に実行する処理が以下
+    # Addressオブジェクトが関連づけられている場合に、customerオブジェクトと関連付ける
     self.customer = address.customer if address
   end
+
+  validates :number, presence: true,
+  # 先頭に+記号が0個または1個以上あり(?のメタ文字)、1個以上の数字が並び、マイナス記号1個と1個以上の数字(\d+)の組み合わせが0個以上(*)末尾まで並ぶ
+  format: { with: /\A\+?\d+(-\d+)*\z/, allow_blank: true }
+
 end
